2e05b1ec4db7701dd21f86358bc4bac603a49777
kongil <o_o0122@naver.com>
2019-06-13 20:07:42 +0900

    all

---
+++ app/build/generated/source/apt/debug/com/donsamo/dongsamo/firebase_control/GlideApp.java
@@ -0,0 +1,124 @@
+package com.donsamo.dongsamo.firebase_control;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import java.io.File;
+import java.lang.Deprecated;
+import java.lang.String;
+
+/**
+ * The entry point for interacting with Glide for Applications
+ *
+ * <p>Includes all generated APIs from all
+ * {@link com.bumptech.glide.annotation.GlideExtension}s in source and dependent libraries.
+ *
+ * <p>This class is generated and should not be modified
+ * @see Glide
+ */
+public final class GlideApp {
+  private GlideApp() {
+  }
+
+  /**
+   * @see Glide#getPhotoCacheDir(Context)
+   */
+  @Nullable
+  public static File getPhotoCacheDir(Context arg0) {
+    return Glide.getPhotoCacheDir(arg0);
+  }
+
+  /**
+   * @see Glide#getPhotoCacheDir(Context, String)
+   */
+  @Nullable
+  public static File getPhotoCacheDir(Context arg0, String arg1) {
+    return Glide.getPhotoCacheDir(arg0, arg1);
+  }
+
+  /**
+   * @see Glide#get(Context)
+   */
+  @NonNull
+  public static Glide get(Context arg0) {
+    return Glide.get(arg0);
+  }
+
+  /**
+   * @see Glide#init(Glide)
+   */
+  @Deprecated
+  @VisibleForTesting
+  @SuppressLint("VisibleForTests")
+  public static void init(Glide glide) {
+    Glide.init(glide);
+  }
+
+  /**
+   * @see Glide#init(Context, GlideBuilder)
+   */
+  @VisibleForTesting
+  @SuppressLint("VisibleForTests")
+  public static void init(Context arg0, GlideBuilder arg1) {
+    Glide.init(arg0, arg1);
+  }
+
+  /**
+   * @see Glide#tearDown()
+   */
+  @VisibleForTesting
+  @SuppressLint("VisibleForTests")
+  public static void tearDown() {
+    Glide.tearDown();
+  }
+
+  /**
+   * @see Glide#with(Context)
+   */
+  public static GlideRequests with(Context arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+
+  /**
+   * @see Glide#with(Activity)
+   */
+  public static GlideRequests with(Activity arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+
+  /**
+   * @see Glide#with(FragmentActivity)
+   */
+  public static GlideRequests with(FragmentActivity arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+
+  /**
+   * @see Glide#with(Fragment)
+   */
+  public static GlideRequests with(Fragment arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+
+  /**
+   * @see Glide#with(Fragment)
+   */
+  public static GlideRequests with(android.app.Fragment arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+
+  /**
+   * @see Glide#with(View)
+   */
+  public static GlideRequests with(View arg0) {
+    return (GlideRequests) Glide.with(arg0);
+  }
+}
--- app/src/main/java/com/dongsamo/dongsamo/StoreActivity.java
+++ app/src/main/java/com/dongsamo/dongsamo/StoreActivity.java
@@ -1,8 +1,11 @@
 package com.dongsamo.dongsamo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,9 +24,17 @@ public class StoreActivity extends AppCompatActivity {
         store_text.setMovementMethod(new ScrollingMovementMethod());
 
         //kong todo 69 StoreRecyclerViewAdapter에서 인텐트로 보낸 store_name 정보 띄우기.
-        Store s = (Store) getIntent().getSerializableExtra("store");
-        //store_text.setText(getIntent().getStringExtra("store_name"));
-        store_text.setText(getIntent().getStringExtra(s.getName()));
+        //Store s = (Store) getIntent().getSerializableExtra("store");
+//        Intent received = new Intent();
+//        Store s = new Store(received.getStringExtra("store_name"), received.getStringExtra("store_img_url"),  received.getDoubleExtra("store_heart",0), received.getDoubleExtra("store_distance",0), received.getBooleanExtra("store_is_heart", false));
+
+//        Log.d("TAG", s.getUrl());
+
+//        store_image.setImageURI(Uri.parse(s.getUrl()));
+
+        Log.d("TAG", getIntent().getStringExtra("store_img_url"));
+        store_text.setText(getIntent().getStringExtra("store_name"));
+        //store_text.setText(getIntent().getStringExtra(s.getName()));
     }
 
     private void setup(){
